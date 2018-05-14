9abe962a05ec06268049ed93e18ace258c07f9a2
robert.donovan-at-344457656106 <robert.donovan@gsa.gov>
2018-05-14 15:05:30 +0000

    Terraform codebuild

---
+++ terraform/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_version = ">= 0.9.3"
+}
+
+data "aws_caller_identity" "default" {}
+
+data "aws_region" "default" {
+  current = true
+}
+
+module "build" {
+    source              = "git::https://github.com/cloudposse/terraform-aws-cicd.git?ref=master"
+    namespace           = "global"
+    name                = "test"
+    stage               = "staging"
+
+    # Enable the pipeline creation
+    enabled             = "true"
+
+    # Application repository on GitHub
+    #github_oauth_token  = "(Optional) <GitHub Oauth Token with permissions to access private repositories>"
+    repo_owner          = "GSA-Briar-Patch"
+    repo_name           = "Amazon_Linux_2"
+    branch              = "master"
+
+    # http://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref.html
+    # http://docs.aws.amazon.com/codebuild/latest/userguide/build-spec-ref.html
+    build_image         = "aws/codebuild/docker:1.12.1"
+    build_compute_type  = "BUILD_GENERAL1_SMALL"
+
+    # These attributes are optional, used as ENV variables when building Docker images and pushing them to ECR
+    # For more info:
+    # http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html
+    # https://www.terraform.io/docs/providers/aws/r/codebuild_project.html
+    privileged_mode     = "true"
+    aws_region          = "us-east-1"
+    #aws_account_id      = "xxxxxxxxxx"
+    #image_repo_name     = "ecr-repo-name"
+    image_tag           = "latest"
+}
